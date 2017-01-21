21b8180d825430df5b87e8945884b1fa5a71b8e7
Kamau Vassall <kamau.vassall@gmail.com>
2017-01-21 12:31:50 -0600

    Get main menu buttons hooked up

--- Ripple Effect/Assets/Scripts/MainMenuScreen.cs
+++ Ripple Effect/Assets/Scripts/MainMenuScreen.cs
@@ -1,11 +1,13 @@
 ﻿
 using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 [DisallowMultipleComponent]
-public class MainMenuScreen : MonoBehaviour 
+public class MainMenuScreen : MonoBehaviour
 {
-    #region Singleton
+	#region Singleton
 
 	public static MainMenuScreen Instance {
 		get {
@@ -25,10 +27,10 @@ public class MainMenuScreen : MonoBehaviour
 	private static MainMenuScreen sInstance;
 
 	#endregion
-	
+
 	#region Enums and Constants
 
-	#endregion 
+	#endregion
 
 	#region Events
 
@@ -36,8 +38,20 @@ public class MainMenuScreen : MonoBehaviour
 
 	#region Properties
 
-	#endregion 
-	
+	[Tooltip ("")]
+	[SerializeField]
+	private string m_MainGameScene = "main";
+
+	[Tooltip ("")]
+	[SerializeField]
+	private Button m_StartGame;
+
+	[Tooltip ("")]
+	[SerializeField]
+	private Button m_ExitGame;
+
+	#endregion
+
 	#region Inspectables
 
 	#endregion
@@ -46,19 +60,36 @@ public class MainMenuScreen : MonoBehaviour
 
 	#endregion
 
-    #region Monobehaviours
+	#region Monobehaviours
 
-	protected void Awake () 
+	protected void Awake ()
 	{
 		if (sInstance == null) {
 			sInstance = this;
 		}
 	}
 
-	protected void Start () 
+	protected void Start ()
 	{
+		if (string.IsNullOrEmpty (m_MainGameScene)) {
+			Debug.LogWarning ("m_MainGameScene is null");
+		}
+		if (m_StartGame == null) {
+			Debug.LogWarning ("m_StartGame is null");
+		} else {
+			m_StartGame.onClick.AddListener (() => {
+				StartGame ();
+			});
+		}
+		if (m_ExitGame == null) {
+			Debug.LogWarning ("m_ExitGame is null");
+		} else {
+			m_ExitGame.onClick.AddListener (() => {
+				ExitGame ();
+			});
+		}
 	}
-	
+
 	protected void Update ()
 	{
 	}
@@ -83,6 +114,22 @@ public class MainMenuScreen : MonoBehaviour
 	#endregion
 
 	#region Private Methods
+
+	private void StartGame ()
+	{
+		SceneManager.LoadScene (m_MainGameScene);
+	}
+
+	private void ExitGame ()
+	{
+		#if UNITY_EDITOR
+		// Application.Quit() does not work in the editor so
+		// UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
 
 	#endregion
 }
