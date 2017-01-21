1981a00c99056f2c2b8da9f2f18bf999ebee2242
Kamau Vassall <kamau.vassall@gmail.com>
2017-01-21 02:04:26 -0600

    Focus player script

---
+++ Ripple Effect/Assets/Scripts/FocusPlayer.cs
@@ -0,0 +1,81 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+public class FocusPlayer : MonoBehaviour
+{
+	#region Enums and Constants
+
+	#endregion
+
+	#region Events
+
+	#endregion
+
+	#region Properties
+
+	#endregion
+
+	#region Inspectables
+
+	#endregion
+
+	#region Private Member Variables
+
+	#endregion
+
+	#region Monobehaviours
+
+	protected void Awake ()
+	{
+	}
+
+	protected void Start ()
+	{
+	}
+
+	protected void Update ()
+	{
+	}
+
+	protected void OnEnable ()
+	{
+	}
+
+	protected void OnDisable ()
+	{
+	}
+
+	#endregion
+
+	#region Trigger Collider
+
+	protected void OnTriggerEnter (Collider coll)
+	{
+		FocusCamera player = coll.gameObject.GetComponent<FocusCamera> ();
+		if (player != null) {
+			FocusCamera fcamera = player.GetComponent<FocusCamera> ();
+			fcamera.enabled = true;
+		}
+	}
+
+	protected void OnTriggerExit (Collider coll)
+	{
+		FocusCamera player = coll.gameObject.GetComponent<FocusCamera> ();
+		if (player != null) {
+			FocusCamera fcamera = player.GetComponent<FocusCamera> ();
+			fcamera.enabled = false;
+		}
+	}
+
+	#endregion
+
+	#region Public Methods
+
+	#endregion
+
+	#region Private Methods
+
+	#endregion
+}
