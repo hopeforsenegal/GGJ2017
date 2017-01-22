43b0f7da9807b912a1586caa8eb76c6e9f685804
Kamau Vassall <kamau.vassall@gmail.com>
2017-01-22 00:49:07 -0600

    Back to compiling

--- Ripple Effect/Assets/Scripts/Door.cs
+++ Ripple Effect/Assets/Scripts/Door.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 using System.Collections;
 
 [DisallowMultipleComponent]
-[RequireComponent( typeof( Animator ) )]
-[RequireComponent( typeof( AudioSource ) )]
-[RequireComponent( typeof( Interactable ) )]
+[RequireComponent (typeof(Animator))]
+[RequireComponent (typeof(AudioSource))]
+[RequireComponent (typeof(Interactable))]
 public class Door : MonoBehaviour
 {
 	#region Enums and Constants
@@ -27,6 +27,7 @@ public class Door : MonoBehaviour
 	#endregion
 
 	#region Private Member Variables
+
 	private AudioSource[] m_Sources;
 
 	private Animator m_Animator;
@@ -41,9 +42,9 @@ public class Door : MonoBehaviour
 
 	protected void Awake ()
 	{
-		m_Sources = GetComponents<AudioSource>();
-		m_Interactable = GetComponent<Interactable>();
-		m_Animator = GetComponent<Animator>();
+		m_Sources = GetComponents<AudioSource> ();
+		m_Interactable = GetComponent<Interactable> ();
+		m_Animator = GetComponent<Animator> ();
 		Interactable.DoInteractEvent += Interactable_DoInteractEvent;
 	}
 
@@ -68,14 +69,12 @@ public class Door : MonoBehaviour
 
 	#region Public Methods
 
-	public void PlaySound( Object audio )
+	public void PlaySound (Object audio)
 	{
-		foreach ( AudioSource source in m_Sources )
-		{
-			if ( !source.isPlaying )
-			{
+		foreach (AudioSource source in m_Sources) {
+			if (!source.isPlaying) {
 				source.clip = audio as AudioClip;
-				source.Play();
+				source.Play ();
 				break;
 			}
 		}
@@ -85,32 +84,28 @@ public class Door : MonoBehaviour
 
 	#region Private Methods
 
-	private void Interactable_DoInteractEvent( Interactable i )
+	private void Interactable_DoInteractEvent (Interactable i)
 	{
-		if ( m_Interactable != null && m_Interactable.ID == i.ID )
-		{
-			if ( m_Animator != null && m_Animator.isActiveAndEnabled )
-			{
-				if ( !m_IsDoorOpen )
-				{
-					m_Animator.SetBool( "OpenDoor", true );
+		if (m_Interactable != null && m_Interactable.ID == i.ID) {
+			if (m_Animator != null && m_Animator.isActiveAndEnabled) {
+				if (!m_IsDoorOpen) {
+					m_Animator.SetBool ("OpenDoor", true);
 					m_IsDoorOpen = true;
-				} 
-				else
-				{
-					m_Animator.SetBool( "OpenDoor", false );
+				} else {
+					m_Animator.SetBool ("OpenDoor", false);
 					m_IsDoorOpen = false;
 				}
-				m_Animator.Update( 0.0f );
+				m_Animator.Update (0.0f);
 			}
 		}
 
-		GameController gameController;
-		if (GameController.TryGetInstance (out gameController)) {
-			gameController.CurrentRoom.transform.position = m_Transform.position;
-			gameController.CurrentRoom.transform.rotation = m_Transform.rotation;
-			gameController.CurrentRoom.transform.localScale = m_Transform.localScale;
-		}
+//		GameController gameController;
+//		if (GameController.TryGetInstance (out gameController)) {
+//			gameController.CurrentRoom.transform.position = m_Transform.position;
+//			gameController.CurrentRoom.transform.rotation = m_Transform.rotation;
+//			gameController.CurrentRoom.transform.localScale = m_Transform.localScale;
+//		}
 	}
+
 	#endregion
 }
