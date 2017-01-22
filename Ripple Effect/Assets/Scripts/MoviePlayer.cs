f9b9fd16ec3ca1f6e054103d72e7df478598d8f4
Kamau Vassall <kamau.vassall@gmail.com>
2017-01-21 22:23:28 -0600

    Main menu

---
+++ Ripple Effect/Assets/Scripts/MoviePlayer.cs
@@ -0,0 +1,77 @@
+﻿
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.Events;
+
+[DisallowMultipleComponent]
+public class MoviePlayer : MonoBehaviour
+{
+	#region Enums and Constants
+
+	#endregion
+
+	#region Events
+
+	#endregion
+
+	#region Properties
+
+	#endregion
+
+	#region Inspectables
+
+	#endregion
+
+	#region Private Member Variables
+
+	private MovieTexture m_MovieTexture;
+
+	#endregion
+
+	#region Monobehaviours
+
+	protected void Awake ()
+	{
+		RawImage rim = GetComponent<RawImage> ();
+		m_MovieTexture = (MovieTexture)rim.mainTexture;
+	}
+
+	protected void Start ()
+	{        
+		m_MovieTexture.Play ();
+	}
+
+	protected void Update ()
+	{
+	}
+
+	protected void OnEnable ()
+	{
+	}
+
+	protected void OnDisable ()
+	{
+	}
+
+	protected void OnBecameVisible ()
+	{
+		enabled = true;
+	}
+
+	protected void OnBecameInvisible ()
+	{
+		enabled = false;
+	}
+
+	#endregion
+
+	#region Public Methods
+
+	#endregion
+
+	#region Private Methods
+
+	#endregion
+}
